64d33d8a3eefe5a3f2a74ca9a9b35de9746f595d
Daniel Javadi Moth <56803330+djmoth@users.noreply.github.com>
2022-03-19 00:35:27 +0100

    Defined Task<TResult> for a task with a return type

--- Moth.Tasks/ITask.cs
+++ Moth.Tasks/ITask.cs
@@ -10,4 +10,28 @@
         /// </summary>
         void Run ();
     }
+
+    /// <summary>
+    /// Interface providing a <see cref="Run"/> method for executing task code, returning a result of type <typeparamref name="TResult"/>.
+    /// </summary>
+    /// <typeparam name="TResult">Return type of <see cref="Run"/> method.</typeparam>
+    public interface ITask<out TResult> : ITask
+    {
+        /// <summary>
+        /// Task code to run.
+        /// </summary>
+        TResult Run ();
+
+        void ITask.Run() => Run ();
+    }
+
+    /*public interface ITask<in TInput> : ITask
+    {
+        /// <summary>
+        /// Task code to run.
+        /// </summary>
+        TOutput Run ();
+
+        void ITask.Run () => Run ();
+    }*/
 }
---
+++ Moth.Tasks/TaskWithResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Moth.Tasks
+{
+    internal struct TaskWithResult<TProducer, TConsumer> : ITask where TProducer : ITask<>
+    {
+        private Task t;
+
+        public void Run ()
+        {
+            
+
+        }
+        struct Task : ITask<Void>
+        {
+            public void Run () => 2;
+        }
+    }
+}
