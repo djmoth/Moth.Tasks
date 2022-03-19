43cf7044a07b611670d5c60290e62ab7f5d0d02f
Daniel Javadi Moth <56803330+djmoth@users.noreply.github.com>
2022-03-20 00:14:12 +0100

    Added T4 template for generating 16 variations of DelegateTask<T1, ...> tasks. Started work on second template for generating corresponding TaskQueue.Enqueue<T1, ...> (Action<T1, ...>, arg1, ...) methods.

--- Moth.Tasks/ITask.cs
+++ Moth.Tasks/ITask.cs
@@ -12,26 +12,43 @@
     }
 
     /// <summary>
-    /// Interface providing a <see cref="Run"/> method for executing task code, returning a result of type <typeparamref name="TResult"/>.
+    /// Interface providing a <see cref="Run"/> method for executing chained task code, passing on a result of type <typeparamref name="TOut"/> to the next task in the chain.
     /// </summary>
-    /// <typeparam name="TResult">Return type of <see cref="Run"/> method.</typeparam>
-    public interface ITask<out TResult> : ITask
+    /// <typeparam name="TOut">Return type of <see cref="Run"/> method.</typeparam>
+    public interface ITaskOut<TOut>
     {
         /// <summary>
         /// Task code to run.
         /// </summary>
-        TResult Run ();
-
-        void ITask.Run() => Run ();
+        /// <returns>Data to pass on to next task in the chain.</returns>
+        TOut Run ();
     }
 
-    /*public interface ITask<in TInput> : ITask
+    /// <summary>
+    /// Interface providing a <see cref="Run"/> method for executing chained task code, taking an argument of type <typeparamref name="TIn"/> from the previous task in the chain.
+    /// </summary>
+    /// <typeparam name="TIn">Argument type for <see cref="Run"/> method.</typeparam>
+    public interface ITaskIn<TIn>
     {
         /// <summary>
         /// Task code to run.
         /// </summary>
-        TOutput Run ();
+        /// <param name="input">Input data from previous task in the chain.</param>
+        void Run (TIn input);
+    }
 
-        void ITask.Run () => Run ();
-    }*/
+    /// <summary>
+    /// Interface providing a <see cref="Run"/> method for executing chained task code, taking an argument of type <typeparamref name="TIn"/> from the previous task, and passing on a result of type <typeparamref name="TOut"/> to the next task in the chain.
+    /// </summary>
+    /// <typeparam name="TIn">Return type of <see cref="Run"/> method.</typeparam>
+    /// <typeparam name="TOut">Argument type for <see cref="Run"/> method.</typeparam>
+    public interface ITaskInOut<TIn, TOut>
+    {
+        /// <summary>
+        /// Task code to run.
+        /// </summary>
+        /// <param name="input">Input data from previous task in the chain.</param>
+        /// <returns>Data to pass on to next task in the chain.</returns>
+        TOut Run (TIn input);
+    }
 }
